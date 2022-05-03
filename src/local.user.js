49d37464f7a018c1ad73b8eb4d8cdb40dfe2b2c8
nokiam9 <sj0225@icloud.com>
2022-05-03 22:58:20 +0800

    fix: 补充注释信息

--- src/local.user.js
+++ src/local.user.js
@@ -1,12 +1,19 @@
 // ==UserScript==
-// @name          f6rp-local
-// @grant         unsafeWindow
+// @name            f6rp-local
+// @description     Forester Plugin, scrapy notices from b2b.10086.cn
+// @grant           unsafeWindow
+// @author          sj0225@icloud.com
 // @noframes
-// @include       https://b2b.10086.cn*
-// @include       http://b2b.10086.cn*
-// @include       http://f6rp.caogo.cn*
-// @include       https://127.0.0.1*
-// @include       http://127.0.0.1*
+// @include         https://b2b.10086.cn*
+// @include         http://b2b.10086.cn*
+// @include         http://f6rp.caogo.cn*
+// @include         https://127.0.0.1*
+// @include         http://127.0.0.1*
+// @grant           GM_xmlhttpRequest
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_listValues
+// @grant           GM_deleteValue
 // ==/UserScript==
 javascript: void(function() {
     unsafeWindow['F6RP_URL'] = 'https://127.0.0.1:7302/';
