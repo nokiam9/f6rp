9434598822cd74027333d182794d519327870ce7
nokiam9 <sj0225@icloud.com>
2022-05-04 13:12:38 +0800

    feat: new data.js

---
+++ src/app/data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// 任务队列中的任务对象
+ class F6rpRequest{
+    constructor({url ,html}={}) {
+        this.url=url;
+        this.html =html;
+        this.completed =false;
+    }
+}
+// 页面的任务对象
+class F6rpPageRequest extends PxerRequest{
+    constructor(...argn) {
+        super(...argn);
+        this.type_id = argn[0].type_id;
+    }
+}
+
+// 构造对象PageInfo，对应Page底部的pageination信息
+class F6rpPageInfo {
+    constructor({spider, type_id ,total, current_page, page_size=20, records_in_page}) {
+        this.spider = spider;       // Todo：replace with UID
+        this.type_id = type_id;     // Todo：replace with url
+        this.total = total;
+        this.current_page = current_page;   // key info
+        this.page_size = page_size;
+        this.records_in_page = records_in_page;
+    }
+}
+
+// 构造对象Notice，对应列表的一行信息
+class F6rpPageLine {
+    constructor({nid, source_ch, notice_type, title, publish_date}) {
+        this.nid = nid;                     // primary key！
+        this.source_ch = source_ch;
+        this.notice_type = notice_type;
+        this.title = title;
+        this.publish_date = publish_date;
+    }
+}
